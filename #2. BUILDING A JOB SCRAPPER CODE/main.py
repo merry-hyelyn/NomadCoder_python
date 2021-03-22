cdc9e27e4f69b0346b324cf8a0f406074909515c
merry-hyelyn <h1335@naver.com>
2021-03-22 23:48:53 +0900

    #2.7 code update

--- #2. BUILDING A JOB SCRAPPER CODE/main.py
+++ #2. BUILDING A JOB SCRAPPER CODE/main.py
@@ -8,6 +8,8 @@
 # -------------------------------------------
 
 from indeed import extract_indeed_pages, extract_indeed_jobs
+from save import save_to_file
 
 last_indeed_page = extract_indeed_pages()
 indeed_jobs = extract_indeed_jobs(last_indeed_page)
+# save_to_file()
